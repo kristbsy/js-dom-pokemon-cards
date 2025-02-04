a2b17f0573e6b4efebb46e7006c79e6f637040ed
Kristian Sylte <kristianbsylte@gmail.com>
2025-02-04 12:01:36 +0100

    extension 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-for (card of data) {
+for (const [index, card] of data.entries()) {
   const li = document.createElement("li");
   li.className = "card";
+  li.id = `card_${index}`;
 
   const h = document.createElement("h2");
   h.className = "card--title";
   const img = document.createElement("img");
   img.width = "256";
   img.className = "card--img";
-  img.src = card.sprites.front_default;
+  img.src = card.sprites.other["official-artwork"].front_default;
 
   const ul = document.createElement("ul");
   ul.className = "card--text";
@@ -16,7 +17,7 @@ for (card of data) {
     const name = stat.stat.name;
     const value = stat.base_stat;
     const statString = `${name}: ${value}`;
-    const sli= document.createElement("li");
+    const sli = document.createElement("li");
     sli.innerText = statString;
     ul.appendChild(sli);
   }
@@ -26,5 +27,25 @@ for (card of data) {
   li.appendChild(ul);
 
   const cardList = document.querySelector(".cards");
+  li.addEventListener("click", () => cardClicked(index));
   cardList.appendChild(li);
+}
+
+function cardClicked(id) {
+  const images = [
+    data[id].sprites.back_default,
+    data[id].sprites.back_female,
+    data[id].sprites.back_shiny,
+    data[id].sprites.back_shiny_female,
+    data[id].sprites.front_default,
+    data[id].sprites.front_female,
+    data[id].sprites.front_shiny,
+    data[id].sprites.front_shiny_female,
+    data[id].sprites.other.dream_world.front_default,
+    data[id].sprites.other.dream_world.front_female,
+    data[id].sprites.other["official-artwork"].front_default,
+  ].filter(el => el !== null);
+  const card = document.querySelector(`#card_${id}`);
+  let img = card.querySelector("img");
+  img.src = images[(images.findIndex((i) => i == img.src) + 1) % images.length];
 }
