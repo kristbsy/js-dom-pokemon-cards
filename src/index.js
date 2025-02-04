02e4cdcd42fe9552e28590203fa1ebc0d332544e
Kristian Sylte <kristianbsylte@gmail.com>
2025-02-04 10:41:23 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+for (card of data) {
+  const li = document.createElement("li");
+  li.className = "card";
 
-console.log(data);
+  const h = document.createElement("h2");
+  h.className = "card--title";
+  const img = document.createElement("img");
+  img.width = "256";
+  img.className = "card--img";
+  img.src = card.sprites.front_default;
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  const ul = document.createElement("ul");
+  ul.className = "card--text";
+
+  for (stat of card.stats) {
+    const name = stat.stat.name;
+    const value = stat.base_stat;
+    const statString = `${name}: ${value}`;
+    const sli= document.createElement("li");
+    sli.innerText = statString;
+    ul.appendChild(sli);
+  }
+
+  li.appendChild(h);
+  li.appendChild(img);
+  li.appendChild(ul);
+
+  const cardList = document.querySelector(".cards");
+  cardList.appendChild(li);
+}
